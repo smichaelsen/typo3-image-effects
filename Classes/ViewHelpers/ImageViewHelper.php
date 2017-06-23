bab63b0776130e31b5265779cd6d7eb03f302e91
Sebastian Michaelsen <sebastian@michaelsen.io>
2017-06-23 23:37:06 +0200

    [BUGFIX] Add missing phpDoc in custom ImageViewHelper

--- Classes/ViewHelpers/ImageViewHelper.php
+++ Classes/ViewHelpers/ImageViewHelper.php
@@ -2,10 +2,32 @@
 namespace Smichaelsen\ImageEffects\ViewHelpers;
 
 use TYPO3\CMS\Core\Resource\Exception\ResourceDoesNotExistException;
+use TYPO3\CMS\Core\Resource\FileInterface;
 use TYPO3\CMS\Core\Resource\FileReference;
+use TYPO3\CMS\Extbase\Domain\Model\AbstractFileFolder;
 
 class ImageViewHelper extends \TYPO3\CMS\Fluid\ViewHelpers\ImageViewHelper
 {
+
+    /**
+     * Resizes a given image (if required) and renders the respective img tag
+     *
+     * @see https://docs.typo3.org/typo3cms/TyposcriptReference/ContentObjects/Image/
+     * @param string $src a path to a file, a combined FAL identifier or an uid (int). If $treatIdAsReference is set, the integer is considered the uid of the sys_file_reference record. If you already got a FAL object, consider using the $image parameter instead
+     * @param string $width width of the image. This can be a numeric value representing the fixed width of the image in pixels. But you can also perform simple calculations by adding "m" or "c" to the value. See imgResource.width for possible options.
+     * @param string $height height of the image. This can be a numeric value representing the fixed height of the image in pixels. But you can also perform simple calculations by adding "m" or "c" to the value. See imgResource.width for possible options.
+     * @param int $minWidth minimum width of the image
+     * @param int $minHeight minimum height of the image
+     * @param int $maxWidth maximum width of the image
+     * @param int $maxHeight maximum height of the image
+     * @param bool $treatIdAsReference given src argument is a sys_file_reference record
+     * @param FileInterface|AbstractFileFolder $image a FAL object
+     * @param string|bool $crop overrule cropping of image (setting to FALSE disables the cropping set in FileReference)
+     * @param bool $absolute Force absolute URL
+     *
+     * @throws \TYPO3\CMS\Fluid\Core\ViewHelper\Exception
+     * @return string Rendered tag
+     */
     public function render($src = null, $width = null, $height = null, $minWidth = null, $minHeight = null, $maxWidth = null, $maxHeight = null, $treatIdAsReference = false, $image = null, $crop = null, $absolute = false)
     {
         if (is_null($src) && is_null($image) || !is_null($src) && !is_null($image)) {
